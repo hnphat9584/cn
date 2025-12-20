af87783a201df9313c1aee11c3071b61676c136c
Huỳnh Nguyên Phát <hnphat0415@gmail.com>
2025-12-20 20:35:27 +0700

    Update ProcessManager.hpp

--- include/modules/ProcessManager.hpp
+++ include/modules/ProcessManager.hpp
@@ -9,10 +9,12 @@
 // Process and Application Management
 class ProcessManager {
 private:
-    static std::string escapeJson(const std::string& str);    
+    static std::string escapeJson(const std::string& str);
+    static std::string utf8_from_wstring(const std::wstring& w);
 public:
     static std::string listProcesses();    
     static std::string startProcess(const std::string& path);    
     static std::string stopProcess(DWORD pid);    
     static std::string listApplications();
+
 };
